fc4771b9336c788b9a7f6ccb4fe4efbbaa83e25e
Tachi Dito <tachidito_7inc@hotmail.com>
2023-07-05 00:51:04 -0600

    validando campos imcompletos o vacios

--- JS/validaciones.js
+++ JS/validaciones.js
@@ -2,18 +2,71 @@ window.addEventListener("load", () => {
   console.log("Page loaded");
 });
 
-const inputBday = document.querySelector("#birth");
+export function validate(input) {
+  const inputType = input.dataset.tipo;
+  if (validators[inputType]) {
+    validators[inputType](input);
+  }
+  if (input.validity.valid) {
+    input.parentElement.classList.remove("input-container--invalid");
+    input.parentElement.querySelector(".input-message-error").innerHTML = "";
+  } else {
+    input.parentElement.classList.add("input-container--invalid");
+    input.parentElement.querySelector(".input-message-error").innerHTML = 
+      showErrorMessages(inputType, input);
+  }
+}
 
-inputBday.addEventListener("blur", (eve) => {
-  validateAge(eve.target);
-});
+const typeErrors = [
+  "valueMissing",
+  "typeMismatch",
+  "patternMismatch",
+  "customError",
+];
+
+const errorMessages = {
+  name: {
+    valueMissing: "Debes de proporcionar un nombre de usuario",
+  },
+  email: {
+    valueMissing: "Debes de proporcionar un email de contacto",
+    typeMismatch: "El correo no es valido",
+  },
+  password: {
+    valueMissing: "Debes de definir una contraseña para tu acceso",
+    patternMismatch:
+      "Al menos 6 caracteres, máximo 12, debe contener una letra minúscula, una letra mayúscula y un número",
+  },
+  birth: {
+    valueMissing: "Debes de comprobar tu mayoría de edad",
+    customError: "Debes de tener al menos 18 años de edad",
+  },
+};
+
+const validators = {
+  birth: (input) => validateAge(input),
+};
+
+function showErrorMessages(inputType, input) {
+  let message = "";
+  typeErrors.forEach((error) => {
+    if (input.validity[error]) {
+      // console.log(inputType, error);
+      // console.log(input.validity[error]);
+      // console.log(errorMessages[inputType][error]);
+      message = errorMessages[inputType][error];
+    }
+  });
+  console.log(message)
+  return message;
+}
+
 
 function validateAge(input) {
-  const clientAge = new Date(input.value);
+  const clientDate = new Date(input.value);
   let message = "";
-  legalAge(clientAge);
 
-  if (!legalAge(clientAge)) {
+  if (!legalAge(clientDate)) {
     message = "Debes de tener al menos 18 años de edad";
   }
   input.setCustomValidity(message);
@@ -25,7 +78,7 @@ function legalAge(date) {
   const compareDates = new Date(
     date.getUTCFullYear() + 18,
     date.getUTCMonth(),
-    date.getUTCDay()
+    date.getUTCDate()
   );
-  return today >= compareDates;
+  return compareDates <= today;
 }
